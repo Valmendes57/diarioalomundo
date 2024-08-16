454442a784001b1c998130005dd13547a6ddbd2c
= <=>
2024-08-16 17:49:18 -0300

    final ok

---
+++ aluno.py
@@ -0,0 +1,6 @@
+class Aluno():
+    def ___init__(self,ra,nome,tempoestudo,rendafamiliar):
+        self.ra = ra
+        self.nome = nome
+        self.tempoestudo = tempoestudo
+        self.rendafamiliar = rendafamiliar
